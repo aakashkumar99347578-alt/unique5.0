5e562272c3db08cc4bddcc6aea4f9acc407c8820
aakash <aakashkumar99347578@gmail.com>
2026-06-13 19:12:11 +0530

    i solved problem , problem is take a  4-digit number and check if the first and last digits are equal.

---
+++ practice_for_logic_building.py/problem21.py
@@ -0,0 +1,16 @@
+#problem statment:-Take a 4-digit number and check if the first and last digits are equal. 
+
+
+#solution
+
+num = int(input("Enter a 4-digit number: "))
+
+first = num // 1000   
+last = num % 10    
+
+if first == last:
+    print("The first and last digits are equal.")
+
+    
+else:
+    print("The first and last digits are not equal.")
