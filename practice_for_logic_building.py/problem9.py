7b2f4dea9ff8d0217fe86758278ad921d72c0cd9
aakash <aakashkumar99347578@gmail.com>
2026-06-13 14:36:25 +0530

    i solved problem , problem is character and check if it’s a vowel or consonant

---
+++ practice_for_logic_building.py/problem9.py
@@ -0,0 +1,21 @@
+# problem statment :- Take a character and check if it’s a vowel or consonant.
+
+
+#solution
+
+
+def check_character_vowel_constant(character):
+
+
+    if character=="a" or "e" or "i" or "o" or "u":
+        print(character,"is a vowel")
+
+
+    else:
+        print(character,"is a consonant")
+
+check_character_vowel_constant("a")
+check_character_vowel_constant("z")
+
+
+
