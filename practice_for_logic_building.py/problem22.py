8e61d77b0735c8dc0a087fee28a933c518e19431
aakash <aakashkumar99347578@gmail.com>
2026-06-13 19:19:12 +0530

    i solved problem , problem is  Check if a number is a multiple of 7 or ends with 7.

---
+++ practice_for_logic_building.py/problem22.py
@@ -0,0 +1,12 @@
+#problem statment :- Check if a number is a multiple of 7 or ends with 7. 
+
+
+#solution
+
+num=int(input("enter a number"))
+
+if num %7==0 or num%7==7:
+    print("condition satisfied")
+
+else:
+    print("condition not satisfied")
