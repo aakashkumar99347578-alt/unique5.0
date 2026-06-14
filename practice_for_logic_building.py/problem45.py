9a8c0597b725c4d450cb9977b315e21fab285198
aakash <aakashkumar99347578@gmail.com>
2026-06-14 12:36:21 +0530

    i solved problem , problem is Count  many elements are positive, negative, or zero. and Count how many elements are even and odd.

---
+++ practice_for_logic_building.py/problem45.py
@@ -0,0 +1,35 @@
+#problem statment:- Count  many elements are positive, negative, or zero. and Count how many elements are even and odd.
+
+#solution
+
+
+n = int(input("Enter number of elements: "))
+arr = []
+
+for i in range(n):
+    arr.append(int(input("Enter element: ")))
+pos = neg = zero = 0
+
+for x in arr:
+    if x > 0:
+        pos += 1
+    elif x < 0:
+        neg += 1
+    else:
+        zero += 1
+
+print("Positive:", pos)
+print("Negative:", neg)
+print("Zero:", zero)
+
+
+even = odd = 0
+
+for x in arr:
+    if x % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Even:", even)
+print("Odd:", odd)
