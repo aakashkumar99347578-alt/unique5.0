55613465754941d526c23eed7ca1762e90dde08f
aakash <aakashkumar99347578@gmail.com>
2026-06-14 08:04:01 +0530

    i solved problem , problem is  Print the squares of numbers from 1 to n. and Print cubes of numbers from 1 to n

---
+++ practice_for_logic_building.py/problem35.py
@@ -0,0 +1,16 @@
+# problem statment :-  Print the squares of numbers from 1 to n. and Print cubes of numbers from 1 to n
+
+
+#solution
+
+num=int(input("enter the number for you want to print square where"))
+
+for i in range(1,num+1):
+    print(i*i)
+
+
+
+num=int(input("enter the number for you want to print cube where"))
+
+for i in range(1,num+1):
+    print(i*i*i)
