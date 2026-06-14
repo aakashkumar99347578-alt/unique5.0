132f95e0133798a22bd52ac29637092c98a97ec6
aakash <aakashkumar99347578@gmail.com>
2026-06-14 07:50:39 +0530

    i solve problem , problem is Check if a number is prime or not.

---
+++ practice_for_logic_building.py/problem33.py
@@ -0,0 +1,24 @@
+#problem statment:-  Check if a number is prime or not.
+
+
+#solution 
+
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a Prime Number")
+else:
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Prime Number")
+    else:
+        print("Not a Prime Number")
+
+        
