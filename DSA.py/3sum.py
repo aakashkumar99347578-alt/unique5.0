f9fdf2f291b0d82e8cf7287d0d02fe1e06b26994
aakash <aakashkumar99347578@gmail.com>
2026-06-15 22:35:40 +0530

    i solved problem , problem is Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0. Notice that the solution set must not contain duplicate triplets.

---
+++ DSA.py/3sum.py
@@ -0,0 +1,45 @@
+#problem statment:- Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+#Notice that the solution set must not contain duplicate triplets.
+
+
+
+#solution:-
+
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res = []
+        n = len(nums)
+
+        for i in range(n - 2):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+
+                elif total > 0:
+                    right -= 1
+
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    left  += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return res
